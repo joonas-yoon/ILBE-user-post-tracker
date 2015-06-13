d50dac171dd096f11f1495d48f4f06eae83388b3
Joona Yoon <joonas.yoon@gmail.com>
2015-06-13 11:46:30 +0900

    Object 클래스

---
+++ src/ArticleInfo.java
@@ -0,0 +1,37 @@
+package ilbe.tracer;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class ArticleInfo extends DefaultMutableTreeNode {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String title  = null;
+	private String author = null;
+	private String date   = null;
+	private String url    = null;
+	
+	public ArticleInfo(){
+		title = null;
+		author= null;
+		date  = null;
+		url   = null;
+	}
+	public ArticleInfo(String title, String author, String date){
+		this.title  = title;
+		this.author = author;
+		this.date   = date;
+	}
+	
+	public void setUrl(String url){
+		this.url = url;
+	}
+	
+	public String getUrl(){
+		return url;
+	}
+	
+	public String toString(){
+		return title;
+	}
+}
