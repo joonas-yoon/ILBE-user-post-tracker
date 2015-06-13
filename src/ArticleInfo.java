812a5b3ddf7736789122f0f1e87a334208b1a09d
Joona Yoon <joonas-yoon@users.noreply.github.com>
2015-06-13 17:52:57 +0900

    Update ArticleInfo.java

--- src/ArticleInfo.java
+++ src/ArticleInfo.java
@@ -3,35 +3,37 @@ package ilbe.tracer;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public class ArticleInfo extends DefaultMutableTreeNode {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	private String title  = null;
+
+	private String title = null;
+	@SuppressWarnings("unused")
 	private String author = null;
-	private String date   = null;
-	private String url    = null;
-	
-	public ArticleInfo(){
+	private String date = null;
+	private String url = null;
+
+	public ArticleInfo() {
 		title = null;
-		author= null;
-		date  = null;
-		url   = null;
+		author = null;
+		date = null;
+		url = null;
 	}
-	public ArticleInfo(String title, String author, String date){
-		this.title  = title;
+
+	public ArticleInfo(String title, String author, String date) {
+		this.title = title;
 		this.author = author;
-		this.date   = date;
+		this.date = date;
 	}
-	
-	public void setUrl(String url){
+
+	public void setUrl(String url) {
 		this.url = url;
 	}
-	
-	public String getUrl(){
+
+	public String getUrl() {
 		return url;
 	}
-	
-	public String toString(){
-		return title;
+
+	public String toString() {
+		return "[" + date + "] " + title;
 	}
 }
